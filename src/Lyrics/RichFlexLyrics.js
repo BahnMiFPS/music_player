1d675b2128f1e87b29a9804c2b645ca31d4da84b
BahnMiFPS <87252277+BahnMiFPS@users.noreply.github.com>
2023-02-28 16:48:04 +1100

    2nd

---
+++ src/Lyrics/RichFlexLyrics.js
@@ -0,0 +1,76 @@
+const RichFlexLyrics = `      Go buy a zip of w-, hit the club
+Pay for like ten n- to get in, we crunk, lit in this b-, yeah
+Know we walk around the world
+Steppin' not givin a damn 'bout where our feet land at
+Yeah, get your a- mushed, smooshed (6ix)
+Yeah, 21, the biggest
+Put a n- in the chicken wing
+21, can you do somethin' for me? (21)
+Can you hit a lil' rich flex for me? (21)
+And 21, can you do somethin' for me? (21, 21)
+Drop some bars to my - ex for me
+And 21 (21), can you do somethin" for me? (Yeah)
+Can you talk to the opps necks for me? (Okay)
+21, do your thing, 21, do your thing (21)
+Do your thing, 21, do your thing
+Yellow diamonds in the watch, this sh- cost a lot
+Never send a b- your dot, that's how you get shot
+I DM in Vanish Mode, I do that sh- a lot
+Took her panties off and this b- thicker than a plot
+All my exes ain't nothin', them h- busted
+If my opps ain't rappin', they ass duckin'
+You ain't ready to pull the trigger, don't clutch it
+I know you on your -, baby, can you -?
+I'm a savage (21)
+Smack her booty in Magic (21)
+I'll slap a - with a ratchet
+I might slap a tracker on his whip and get to addin'
+Don't call me on Christmas Eve, b-, call your daddy (21)
+Call your uncle (21), b-, don't call me (21)
+Always in my ear, your h- a flea(
+Why my opps be posting guns and only use they feet? (21)
+Paid like an athlete, I got-
+All you h-
+All of you h- need to remember who y'all talking to
+It's the Slaughter Gang CEO
+I got d- for you if I'm not working, girl
+If I'm busy, then, f- no
+You need to find you someone else to call
+When your bank account get low
+You need to find you someone
+Ayy, ayy, ayy, ayy, ayy
+I'm on that Slaughter Gang sh-, ayy, murder gang sh-
+Ayy, Slaughter Gang sh-, ayy, murder gang sh-
+Ayy, sticks and stones, chrome on chrome
+That's just what a n- on
+Internet clones, got 'em kissin' through the phone
+Clickin' up so they don't feel alone, ayy
+Nan' n- seein' me, I'm Young Money CMB
+I used to roll with CMG, the house is not a BNB
+The bad b- waitin' on a n- like I'm PnD
+I'm steady pushing P, you n- pushing PTSD
+I told her a- to kiss me in the club, f- a TMZ
+I used to want a GMC, when Woe was doing BNE
+We revvin' up and goin' on a run like the DMC
+I layup with her for a couple days, then its BRB
+You rappers like askin' if I f-, when you know we did
+When you know we did
+She came in heels, but she left out on her cozy sh-
+Ayy, I'm livin every 24 like Kobe did
+Shoutout to the 6ix, R.I.P the 8
+Swear this sh- is getting ate, I'm on ten for the cake
+Get a lot of love from 12, but I don't reciprocate
+51 division stay patrolling when it's late
+21 my addy, so the knife is on the gate
+All the dawgs eating off a Baccarat plate
+See Drake and they underestimate
+Take it from a vet, that's a rookie a- mistake, ayy
+Ah, what, what
+Slaughter Gang sh-, ayy, murder gang sh-, ayy
+Slaughter Gang sh-, ayy, murder gang sh-, ayy
+(Slaughter gang sh-, ayy, murder gang sh-, ayy)
+(And you got 'em)
+Boy, look, you the m- man
+Boy, you, ooh, you is the man, you hear me?`
+
+export default RichFlexLyrics
